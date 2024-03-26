bb03b07c6a94aa992e5816fd7eedc969822e8a1d
mehwishshakeel <mehwishshakeel>
2024-03-26 22:07:48 +0500

    publish to github

---
+++ Q25.ts
@@ -0,0 +1,9 @@
+let alian_Color = "red";
+//if(condition){ code}
+if(alian_Color === "red"){
+    console.log("Player earned five points");
+
+}
+if (alian_Color === "White"){
+    console.log("Player will be out");
+}
